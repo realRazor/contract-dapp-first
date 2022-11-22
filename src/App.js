65e67f5bc30c2098841a35b53c9573575f8531ec
Talha <yesilyurttalha2534@gmail.com>
2022-11-23 01:18:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import HelloWorld from "./contract/HelloWorld.json"
 function App() {
+  
+  const [address,setAddress] = useState("");
+  useEffect(() => {
+    
+    (async() => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum,"any");
+      
+      await provider.send("eth_requestAccounts",[]);
+
+      const signer = provider.getSigner();
+      const myAddress = await signer.getAddress();
+      setAddress(myAddress);
+      const helloContractAddress = "0x75EC69f26FCa67D62f0bfF34e22364001362699d";
+      const contract = new ethers.Contract(
+        helloContractAddress,
+        HelloWorld.abi,
+        signer
+      );
+      const greeter = await contract.helloWorld();
+      console.log(greeter);
+      
+    })();
+    return () => {
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        First dApp
+        
+        <p>Your Address: {address} </p>
+        
+      </header>  
     </div>
   );
 }
